0848e68df17a1ac8e700d928b64e974dfdf8d750
Dang Thuy Vo <dangthuy112@gmail.com>
2022-12-05 00:08:06 -0600

    Add Success Message for EditBrewery

--- resources/brewery/EditBrewery.vue
+++ resources/brewery/EditBrewery.vue
@@ -11,6 +11,9 @@
                             </li>
                         </ul>
                     </div>
+                    <div class="alert alert-success" v-if="success">
+                        <span>Brewery Successfully Updated!</span>
+                    </div>
                     <fieldset>
                         <div class="row">
                             <div class="col">
@@ -20,14 +23,14 @@
                                     </label>
                                     <input type="text" class="form-control" id="brewery_id"
                                         placeholder="Enter Brewery ID" v-model="brewery.brewery_id"
-                                        v-on:change="clearErrors">
+                                        v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="name" class="form-label mt-4">Name</label>
                                     <input type="text" class="form-control" id="name" placeholder="Enter Name"
-                                        v-model="brewery.name">
+                                        v-model="brewery.name" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -36,14 +39,15 @@
                                 <div class="form-group">
                                     <label for="brewery_type" class="form-label mt-4">Brewery Type</label>
                                     <input type="text" class="form-control" id="brewery_type"
-                                        placeholder="Enter Brewery Type" v-model="brewery.brewery_type">
+                                        placeholder="Enter Brewery Type" v-model="brewery.brewery_type"
+                                        v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="street" class="form-label mt-4">Street</label>
                                     <input type="text" class="form-control" id="street" placeholder="Enter Street"
-                                        v-model="brewery.street">
+                                        v-model="brewery.street" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -52,14 +56,14 @@
                                 <div class="form-group">
                                     <label for="address_2" class="form-label mt-4">Address 2</label>
                                     <input type="text" class="form-control" id="address_2" placeholder="Enter Address 2"
-                                        v-model="brewery.address_2">
+                                        v-model="brewery.address_2" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="address_3" class="form-label mt-4">Address 3</label>
                                     <input type="text" class="form-control" id="address_3" placeholder="Enter Address 3"
-                                        v-model="brewery.address_3">
+                                        v-model="brewery.address_3" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -68,14 +72,14 @@
                                 <div class="form-group">
                                     <label for="city" class="form-label mt-4">City</label>
                                     <input type="text" class="form-control" id="city" placeholder="Enter City"
-                                        v-model="brewery.city">
+                                        v-model="brewery.city" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="state" class="form-label mt-4">State</label>
                                     <input type="text" class="form-control" id="state" placeholder="Enter State"
-                                        v-model="brewery.state">
+                                        v-model="brewery.state" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -84,14 +88,16 @@
                                 <div class="form-group">
                                     <label for="county_province" class="form-label mt-4">County Providence</label>
                                     <input type="text" class="form-control" id="county_province"
-                                        placeholder="Enter County Providence" v-model="brewery.county_providence">
+                                        placeholder="Enter County Providence" v-model="brewery.county_providence"
+                                        v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="postal_code" class="form-label mt-4">Postal Code</label>
                                     <input type="text" class="form-control" id="postal_code"
-                                        placeholder="Enter Postal Code" v-model="brewery.postal_code">
+                                        placeholder="Enter Postal Code" v-model="brewery.postal_code"
+                                        v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -100,7 +106,7 @@
                                 <div class="form-group">
                                     <label for="country" class="form-label mt-4">Country</label>
                                     <input type="text" class="form-control" id="country" placeholder="Enter Country"
-                                        v-model="brewery.country">
+                                        v-model="brewery.country" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
@@ -108,7 +114,7 @@
                                 <div class="form-group">
                                     <label for="phone" class="form-label mt-4">Phone Number</label>
                                     <input type="text" class="form-control" id="phone" placeholder="Enter Phone Number"
-                                        v-model="brewery.phone_number">
+                                        v-model="brewery.phone_number" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -117,14 +123,15 @@
                                 <div class="form-group">
                                     <label for="website_url" class="form-label mt-4">Website URL</label>
                                     <input type="text" class="form-control" id="website_url"
-                                        placeholder="Enter Website URL" v-model="brewery.website_url">
+                                        placeholder="Enter Website URL" v-model="brewery.website_url"
+                                        v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
                                 <div class="form-group">
                                     <label for="longitude" class="form-label mt-4">Longitude</label>
                                     <input type="text" class="form-control" id="longitude" placeholder="Enter Longitude"
-                                        v-model="brewery.longitude">
+                                        v-model="brewery.longitude" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                         </div>
@@ -133,7 +140,7 @@
                                 <div class="form-group">
                                     <label for="latitude" class="form-label mt-4">Latitude</label>
                                     <input type="text" class="form-control" id="latitude" placeholder="Enter Latitude"
-                                        v-model="brewery.latitude">
+                                        v-model="brewery.latitude" v-on:keyup="clearNotifications">
                                 </div>
                             </div>
                             <div class="col">
@@ -170,8 +177,8 @@ export default {
                 longitude: null,
                 latitude: null,
             },
-
             errors: [],
+            success: false
         }
     },
     methods: {
@@ -208,9 +215,8 @@ export default {
                 const url = `/api/edit_brewery/${this.$route.params.id}`
                 await axios.post(url, formData)
                     .then((response) => {
-                        console.log(response);
                         if (response.status = 200) {
-                            alert(response.data.message);
+                            this.success = true;
                         }
                     }).catch((error) => {
                         if (error.response) {
@@ -228,8 +234,9 @@ export default {
                     });
             }
         },
-        clearErrors() {
+        clearNotifications() {
             this.errors = [];
+            this.success = false;
         }
     },
     created() {
